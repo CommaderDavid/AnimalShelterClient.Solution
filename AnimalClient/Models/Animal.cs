5b669632b1520920ab938b5f558701ab93dd50cd
David Boedigheimer <stealth259@gmail.com>
2021-07-21 15:21:56 -0700

    Add models.

---
+++ AnimalClient/Models/Animal.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AnimalClient.Models
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Gender { get; set; }
+        public string Behavior { get; set; }
+        public string DateAdded { get; set; }
+
+        public static List<Animal> GetAnimals()
+        {
+            Task<string> apiCallTask = ApiHelper.GetAll();
+            string result = apiCallTask.Result;
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+            List<Animal> animalList = JsonConvert.DeserializeObject<List<Animal>>(jsonResponse.ToString());
+
+            return animalList;
+        }
+
+        public static Animal GetDetails(int id)
+        {
+            Task<string> apiCallTask = ApiHelper.Get(id);
+            string result = apiCallTask.Result;
+
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            Animal animal = JsonConvert.DeserializeObject<Animal>(jsonResponse.ToString());
+
+            return animal;
+        }
+
+        public async static Task Post(Animal animal)
+        {
+            string jsonReview = JsonConvert.SerializeObject(animal);
+            await ApiHelper.Post(jsonReview);
+        }
+
+        public async static Task Put(Animal animal)
+        {
+            string jsonAnimal = JsonConvert.SerializeObject(animal);
+            await ApiHelper.Put(animal.AnimalId, jsonAnimal);
+        }
+
+        public async static Task Delete(int id)
+        {
+            await ApiHelper.Delete(id);
+        }
+    }
+}
