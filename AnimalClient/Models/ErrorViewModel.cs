b87a19a413e4a59baf9d3eb6d813c5fbef642145
David Boedigheimer <stealth259@gmail.com>
2021-07-21 15:03:05 -0700

    Initial creation of animal shelter client.

---
+++ AnimalClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnimalClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
